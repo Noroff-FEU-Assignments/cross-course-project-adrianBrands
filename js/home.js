faca7e190dfc83d75aca276fd2ef3d0f39d82849
adrianBrands <adrianbrandshaug@hotmail.com>
2022-03-26 18:59:21 +0100

    improving site

--- js/home.js
+++ js/home.js
@@ -24,19 +24,31 @@ window.onload = changeBackground;
 
 const searchIcon = document.querySelector(".search-icon");
 const inputSearch = document.querySelector(".input-search1");
+const hamburger = document.querySelector("#hamburger-menu"); 
+const shopping = document.querySelector(".shopping"); 
+const hamburgerLabel = document.querySelector(".hamburger-label");
+
+
 
 searchIcon.addEventListener("click", () => {
   inputSearch.innerHTML = `<input class="input-search2" type="text" placeholder="search...">`;
   searchIcon.innerHTML = `<img src="images/close.png">`;
+  shopping.style.display="none";
+  hamburgerLabel.style.display="none";
+  
 
   searchIcon.addEventListener("click", () => {
     if (searchIcon.innerHTML === `<img src="images/close.png">`) {
       inputSearch.style.display = "none";
+      shopping.style.display="unset";
+      hamburgerLabel.style.display="unset";
       searchIcon.innerHTML = `<img src="images/icons8-search-24.png">`;
     } else {
       inputSearch.innerHTML = `<input class="input-search2" type="text" placeholder="search...">`;
       searchIcon.innerHTML = `<img src="images/close.png">`;
       inputSearch.style.display = "unset";
+      shopping.style.display="none";
+      hamburgerLabel.style.display="none";
     }
   });
 });
@@ -57,3 +69,24 @@ const x = setInterval(function() {
   sale.innerHTML = `${days} days, and ${hours} hours left of our special sale... 50%`;
 
 }, 1000);
+
+
+
+
+
+hamburger.addEventListener("click", () => {
+  const logo = document.querySelector(".logo"); 
+  if (logo.style.display === "none"){
+    logo.style.display = "block"; 
+  } else {
+    logo.style.display = "none";
+  }
+  
+  
+  
+  
+  
+
+});
+
+
